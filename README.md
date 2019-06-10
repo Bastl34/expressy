3de20e9845c7c6de085644e1918c80a0f320f9a6
Bastian Karge <bastian.karge@gmail.com>
2019-06-10 22:34:55 +0200

    static file routing and proxying

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# expressy
-Simple Node Express based Websever and Proxy
+# Expressy
+Expressy is a simple node express based websever and proxy.
+
+
+## Howto
+* copy `config.json.dist` to `config.json` and apply your server settings
+
+
+## Types
+### static
+```
+{
+    "domain": "testtest1.com",
+    "type": "static",
+    "source": "./test"
+},
+```
+
+### proxy
+```
+{
+    "domain": "testtest2.com",
+    "type": "proxy",
+    "source": "127.0.0.1:8080"
+},
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const proxy = require('express-http-proxy');
+
+const config = require('./config.json');
+
+const HOST_TYPE =
+{
+    static: 1,
+    proxy: 2
+};
+
+const hosts = {};
+
+config.hosts.forEach(host =>
+{
+    if (host.type == 'proxy')
+        host.type = HOST_TYPE.proxy;
+    else
+        host.type = HOST_TYPE.static;
+
+    hosts[host.domain] = host;
+});
+
+express().use((req, res, next) =>
+{
+    if (!(req.hostname in hosts))
+        return res.status(404).send('not found');
+
+    const host = hosts[req.hostname];
+
+    if (host.type == HOST_TYPE.static)
+        return express.static(host.source)(req, res, next);
+    else if (host.type == HOST_TYPE.proxy)
+        return proxy(host.source)(req, res, next);
+    else
+        return res.status(500).send('server error');
+
+}).listen(config.port);
